884c86dff5529d38112fe303fe788e1793e56d88
justym <mm6mm0mm3mm@gmail.com>
2020-04-12 22:55:26 +0900

    builder pattern

---
+++ creational/builder.go
@@ -0,0 +1,77 @@
+package creational
+
+//ManufacturingDirector section
+type ManufacturingDirector struct {
+	builder BuildProcess
+}
+
+func (f *ManufacturingDirector) Construct() {
+	f.builder.SetWheels().SetSeats().SetStructure()
+}
+
+func (f *ManufacturingDirector) SetBuilder(b BuildProcess) {
+	f.builder = b
+}
+
+//VehicleProduct Type Struct
+type VehicleProduct struct {
+	Wheels    int
+	Seats     int
+	Structure string
+}
+
+//BuildProcess
+type BuildProcess interface {
+	SetWheels() BuildProcess
+	SetSeats() BuildProcess
+	SetStructure() BuildProcess
+	Build() VehicleProduct
+}
+
+//CarBuilder section
+type CarBuilder struct {
+	v VehicleProduct
+}
+
+func (c *CarBuilder) SetWheels() BuildProcess {
+	c.v.Wheels = 4
+	return c
+}
+
+func (c *CarBuilder) SetSeats() BuildProcess {
+	c.v.Seats = 5
+	return c
+}
+
+func (c *CarBuilder) SetStructure() BuildProcess {
+	c.v.Structure = "Car"
+	return c
+}
+
+func (c *CarBuilder) Build() VehicleProduct {
+	return c.v
+}
+
+//BikeBuilder section
+type BikeBuilder struct {
+	v VehicleProduct
+}
+
+func (b *BikeBuilder) SetWheels() BuildProcess {
+	b.v.Wheels = 2
+	return b
+}
+
+func (b *BikeBuilder) SetSeats() BuildProcess {
+	b.v.Seats = 1
+	return b
+}
+
+func (b *BikeBuilder) SetStructure() BuildProcess {
+	b.v.Structure = "Bike"
+	return b
+}
+
+func (b *BikeBuilder) Build() VehicleProduct {
+	return b.v
+}
---
+++ creational/builder_test.go
@@ -0,0 +1,44 @@
+package creational
+
+import "testing"
+
+func TestBuilderPattern(t *testing.T) {
+	manufacturingComplex := ManufacturingDirector{}
+
+	carBuilder := &CarBuilder{}
+	manufacturingComplex.SetBuilder(carBuilder)
+	manufacturingComplex.Construct()
+
+	car := carBuilder.Build()
+
+	if car.Wheels != 4 {
+		t.Errorf("Wheels on a car must be 4, but it's %v", &car.Wheels)
+	}
+
+	if car.Seats != 5 {
+		t.Errorf("Seats on a car must be 5, but it's %v", &car.Seats)
+	}
+
+	if car.Structure != "Car" {
+		t.Errorf("Structure must be Car, but it's %v", &car.Structure)
+	}
+
+	bikeBuilder := &BikeBuilder{}
+	manufacturingComplex.SetBuilder(bikeBuilder)
+	manufacturingComplex.Construct()
+
+	bike := bikeBuilder.Build()
+
+	if bike.Wheels != 2 {
+		t.Errorf("Wheels on a car must be 2, but it's %v", &bike.Wheels)
+	}
+
+	if bike.Seats != 1 {
+		t.Errorf("Seats on a bike must be 1, but it's %v", &bike.Seats)
+	}
+
+	if bike.Structure != "Bike" {
+		t.Errorf("Structure must be Bike, but it's %v", &bike.Structure)
+	}
+
+}
